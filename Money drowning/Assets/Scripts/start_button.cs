ff921a8798cae37683b975c72eab35ae597897b9
RoonuiL <72013507+RoonuiL@users.noreply.github.com>
2022-02-12 12:23:12 +0100

    [ADD] Start button functional

--- Money drowning/Assets/Scripts/start_button.cs
+++ Money drowning/Assets/Scripts/start_button.cs
@@ -7,17 +7,26 @@ using UnityEngine;
 
 public class start_button : MonoBehaviour
 {
-    GameObject b_start;  
+    GameObject b_start;
+    GameObject b_setting;
+
     // Start is called before the first frame update
     void Start()
     {
-        b_start = GameObject.Find("B_Start");
-        b_start.SetActive(false);
+        b_start = GameObject.FindWithTag("Start_button");
+        b_setting = GameObject.FindWithTag("Setting_button");
     }
-
     // Update is called once per frame
     void Update()
     {
        
     }
+
+    public void SetInactiveButton()
+    {
+        b_start.SetActive(false);
+        b_setting.SetActive(false);
+    }
+    
 }
+
